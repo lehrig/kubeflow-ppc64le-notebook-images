b10aaf89c5adc4fd0de067f953b2180f2e52c1b8
root <root@edb-b244-bastion.cecc.ihost.com>
2021-02-24 04:53:50 -0500

    Made links clickable

--- post_jupyter_start.sh
+++ post_jupyter_start.sh
@@ -4,11 +4,15 @@ sleep 10
 
 # Elyra Kubeflow runtime: if no runtime is installed, install default runtime
 if [[ "$(elyra-metadata list runtimes | grep -i json | wc -l)" != "1" ]]; then
+  export HOST=$(cat /etc/resolv.conf | grep search | awk '{ print $NF }')
+  export API_ENDPOINT=http://istio-ingressgateway-istio-system.apps.$HOST/pipeline
+  export COS_ENDPOINT=http://minio-service-kubeflow.apps.$HOST
+
   elyra-metadata install runtimes \
        --display_name="DEV Runtime - Kubeflow Pipelines" \
-       --api_endpoint=http://ml-pipeline-ui.kubeflow \
+       --api_endpoint=$API_ENDPOINT \
        --engine=Argo \
-       --cos_endpoint=http://minio-service.kubeflow:9000 \
+       --cos_endpoint=$COS_ENDPOINT \
        --cos_username=minio \
        --cos_password=minio123 \
        --cos_bucket=kf-pipelines-dev \

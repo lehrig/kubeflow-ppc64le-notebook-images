5dba79c7da4c18a91305e9398dbcda9a2ca78c81
Dr. Sebastian Lehrig <github@lehrig.de>
2021-10-07 18:25:37 +0200

    Update post_jupyter_start.sh

--- post_jupyter_start.sh
+++ post_jupyter_start.sh
@@ -21,3 +21,76 @@ if [[ "$(elyra-metadata list runtimes | grep -i json | wc -l)" != "1" ]]; then
        --tags="['kfp', 'v1.3.0', 'dev']" \
        --schema_name=kfp
 fi
+
+# Elyra Kubeflow runtime images: rewire images for ppc64le
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="anaconda" \
+       --display_name="Miniforge 4.10.3 with Python 3.8" \
+       --description="Python v3.8 / Elyra v3.0.0 / Conda v4.10.3" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-anaconda4.10.3" \
+       --tags="['anaconda']" \
+       --replace
+       
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="pandas" \
+       --display_name="Pandas 1.1.1" \
+       --description="Python v3.8 / Elyra v3.0.0 / Conda v4.10.3 / Pandas 1.1.1" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-pandas1.1.1" \
+       --tags="['pandas']" \
+       --replace
+ 
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="pytorch-devel" \
+       --display_name="Pytorch 1.4 with CUDA-devel" \
+       --description="PyTorch 1.4 (with GPU support)" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-pytorch-devel1.4" \
+       --tags="['gpu', 'pytorch']" \
+       --replace
+
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="pytorch-runtime" \
+       --display_name="Pytorch 1.4 with CUDA-runtime" \
+       --description="PyTorch 1.4 (with GPU support)" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-pytorch-runtime1.4" \
+       --tags="['gpu', 'pytorch']" \
+       --replace
+ 
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="r" \
+       --display_name="R Script" \
+       --description="R Script" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-r4" \
+       --tags="['R']" \
+       --replace
+
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="tensorflow_2x_gpu_py3" \
+       --display_name="Tensorflow 2.3.0 with GPU" \
+       --description="TensorFlow 2.3 (with GPU support)" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-tensorflow2.3.0" \
+       --tags="['gpu', 'tensorflow']" \
+       --replace
+
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="tensorflow_2x_py3" \
+       --display_name="Tensorflow 2.3.0" \
+       --description="TensorFlow 2.3" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-tensorflow-cpu2.3.0" \
+       --tags="['tensorflow']" \
+       --replace
+
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="tensorflow_gpu_py3" \
+       --display_name="Tensorflow 1.15.2 with GPU" \
+       --description="TensorFlow 1.15 (with GPU support)" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-tensorflow1.15.2" \
+       --tags="['gpu', 'tensorflow']" \
+       --replace
+
+elyra-metadata install runtime-images --schema_name=runtime-image \
+       --name="tensorflow_py3" \
+       --display_name="Tensorflow 1.15.2" \
+       --description="TensorFlow 1.15" \
+       --image_name="quay.io/ibm/kubeflow-elyra-runtimes-ppc64le:py3.8-tensorflow-cpu1.15.2" \
+       --tags="['tensorflow']" \
+       --replace

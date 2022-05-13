007f50da2313d80aa1f58689c483a0e684ca520d
Dr. Sebastian Lehrig <github@lehrig.de>
2022-05-13 16:42:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,4 +22,5 @@ fi
 
 nohup post_jupyter_start.sh &
 
+sleep 10
 jupyter lab --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
